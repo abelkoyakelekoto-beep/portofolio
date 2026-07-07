70e03441416fcf58549af1a5ccb7c52aed9ae5a2
abelkoyakelekoto-beep <abelkoyakelekoto@gmail.com>
2026-07-07 22:41:26 +0100

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+<a href="https://wa.me/243818779217" class="btn" target="_blank">
+    Discuter sur WhatsApp
+</a>
+
+<a href="cv/CV_Abel_Koyakele_Koto.pdf" class="btn" download>
+    Télécharger mon CV
+</a>
+
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 50) {
+        header.style.background = "#020617";
+    } else {
+        header.style.background = "#111827";
+    }
+});
